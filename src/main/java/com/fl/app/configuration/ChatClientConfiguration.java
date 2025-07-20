58f917e6f3b42695a1426bab755933a52f7f6c87
Fran Larrocca <francisco.larrocca@outlook.com>
2025-07-20 02:50:12 -0300

    feat: added Cassandra as main DB to store chat messages

--- src/main/java/com/fl/app/configuration/ChatClientConfiguration.java
+++ src/main/java/com/fl/app/configuration/ChatClientConfiguration.java
@@ -1,10 +1,15 @@
 package com.fl.app.configuration;
 
+import com.datastax.oss.driver.api.core.CqlSession;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.repository.cassandra.CassandraChatMemoryRepository;
+import org.springframework.ai.chat.memory.repository.cassandra.CassandraChatMemoryRepositoryConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 
 @Configuration
@@ -14,5 +19,15 @@ public class ChatClientConfiguration {
         return builder
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
+    }
+
+    @Bean
+    public CassandraChatMemoryRepository cassandraChatMemory(CqlSession cqlSession) {
+        return CassandraChatMemoryRepository.create(CassandraChatMemoryRepositoryConfig
+                .builder()
+                .withKeyspaceName("ai_chat_memory_keyspace")
+                .withTimeToLive(Duration.ofDays(30))
+                .withCqlSession(cqlSession)
+                .build());
     }
 }
